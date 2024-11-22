67ee23dc432b3fdfc575375895e211946fb339d1
Edy Segura <edysegura@gmail.com>
2024-11-22 09:42:24 -0300

    chore: list posts from database

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express, { Application, Request, Response } from 'express'
+import { MongoClient } from 'mongodb'
 import connectToDatabase from './database'
 
 const app: Application = express()
@@ -9,10 +10,27 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-connectToDatabase(process.env.MONGO_URI as string)
+let dbConnection: MongoClient | null = null
+async function getDbConnection() {
+  if (!dbConnection) {
+    dbConnection = await connectToDatabase(process.env.MONGO_URI as string)
+  }
+  return dbConnection
+}
+
+async function getPosts() {
+  const dbConnection = await getDbConnection()
+  const db = dbConnection.db('instabytes')
+  return db.collection('posts').find().toArray()
+}
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Server running!!')
+})
+
+app.get('/posts', async (req: Request, res: Response) => {
+  const posts = await getPosts()
+  res.json(posts)
 })
 
 app.listen(port, () => {
