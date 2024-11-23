bda11550b670fbd6c4210e82f35027b1b3917cf2
Edy Segura <edysegura@gmail.com>
2024-11-22 23:16:27 -0300

    feat: insert a new post

--- src/model/posts.ts
+++ src/model/posts.ts
@@ -5,7 +5,7 @@ export async function findPosts() {
   return db.collection('posts').find().toArray()
 }
 
-export async function insertPost(post: unknown) {
+export async function insertPost(post: Record<string, unknown>) {
   const db = await getDB()
-  // return db.collection('posts').insertOne(post)
+  return db.collection('posts').insertOne(post)
 }
