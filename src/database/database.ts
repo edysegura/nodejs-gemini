a968eefbcc67e8c547439a91ef7e2c045e2760b4
Edy Segura <edysegura@gmail.com>
2024-11-22 23:20:13 -0300

    refactor: database.ts

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,21 +3,27 @@ import { Db, MongoClient } from 'mongodb'
 let mongoClient: MongoClient | null = null
 let database: Db | null = null
 
-export default async function getDatabase(
-  connectionString: string = process.env.MONGO_URI as string,
-  dbName: string = 'instabytes',
-) {
-  if (mongoClient && database) return database
+async function connectToDatabase(connectionString: string = process.env.MONGO_URI as string) {
+  if (mongoClient) return mongoClient
   try {
     console.log('Connection string: ' + connectionString)
     mongoClient = new MongoClient(connectionString)
     console.log('Connecting to database cluster...')
     await mongoClient.connect()
     console.log('Successfully connected to MongoDB Atlas!')
-    database = mongoClient.db(dbName)
-    return database
+    return mongoClient
   } catch (error) {
     console.error('Database connection failed!', error)
     process.exit(1)
   }
+}
+
+export default async function getDatabase(
+  connectionString: string = process.env.MONGO_URI as string,
+  dbName: string = 'instabytes',
+) {
+  if (database) return database
+  const client = await connectToDatabase(connectionString)
+  database = client.db(dbName)
+  return database
 }
