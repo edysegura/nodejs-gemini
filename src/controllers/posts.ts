cde4bd993c188110e56499f7bd91f4cc0eee896f
Edy Segura <edysegura@gmail.com>
2024-11-25 12:14:00 -0300

    feat: upload image to the local server

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import fs from 'fs/promises'
 import { findPosts, insertPost } from '../model/posts'
 
 export async function listPosts(req: Request, res: Response) {
@@ -10,6 +11,26 @@ export async function createPost(req: Request, res: Response) {
   const newPost = req.body
   try {
     const result = await insertPost(newPost)
+    res.status(201).json(result)
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating post', error })
+  }
+}
+
+export async function imageUpload(req: Request, res: Response) {
+  if (!req.file) {
+    res.status(400).json({ message: 'No image provided' })
+    return
+  }
+  const newPost = {
+    description: '',
+    imgUrl: req.file.originalname,
+    alt: '',
+  }
+  try {
+    const result = await insertPost(newPost)
+    const imageUpdated = `uploads/${result.insertedId}.png`
+    await fs.rename(req.file?.path, imageUpdated)
     res.status(201).json(result)
   } catch (error) {
     res.status(500).json({ message: 'Error creating post', error })
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,9 +1,25 @@
 import { Application } from 'express'
-import { createPost, listPosts } from '../controllers/posts'
+import multer from 'multer'
+import { createPost, imageUpload, listPosts } from '../controllers/posts'
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({ dest: './uploads', storage })
 
 const routes = (app: Application) => {
   app.get('/posts', listPosts)
   app.post('/posts', createPost)
+  app.get('/posts/:id', (req, res) => {
+    res.status(200).json({ message: `Post ID: ${req.params.id}` })
+  })
+  app.post('/upload', upload.single('image'), imageUpload)
 }
 
 export default routes
